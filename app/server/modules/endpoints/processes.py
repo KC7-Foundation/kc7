e2ba83138d5f2bddef74f1ee21143af1a0d9e96e
Emily Hacker <emhacker@microsoft.com>
2022-09-20 14:48:00 -0700

    Added created process values to processevents class, added constants.py including MD5s for legitimate and malicious files, renamed processes class to ProcessEvents class and updated name in Triggers.py

--- app/server/modules/endpoints/processes.py
+++ app/server/modules/endpoints/processes.py
@@ -12,23 +12,57 @@ class Process:
 class ProcessEvent:
     """
     A class that represents the data model for Processes
+    creation_time is the in-game time when the process is created
+    parent_process_name is the name of the initiating process or file
+    parent_process_hash is the hash of the initiating process or file
+    process_commandline is the commandline parameters associated with the created process
+    process_name is the name of the created process
+    process_hash is the hash of the created process
+    hostname is the name of the machine on which these processes occur
 
     Processes will simulate the execution of legitimate and malicious processes on an endpoint.
 
     This functionality is not yet implemented
     """
 
-    def __init__(self, timestamp: float, parent_process_name: str, parent_process_hash: str, process_arguments: str):
+    def __init__(self,
+                timestamp: float,
+                parent_process_name: str,
+                parent_process_hash: str,
+                process_commandline: str,
+                process_name: str,
+                process_hash: str,
+                hostname: str):
 
         self.timestamp = timestamp
         self.parent_process_name = parent_process_name
         self.parent_process_hash = parent_process_hash
-        self.process_arguments = process_arguments
+        self.process_commandline = process_commandline
+        self.process_name = process_name
+        self.process_hash = process_hash
+        self.hostname = hostname
 
     def stringify(self):
         return {
             "timestamp": self.timestamp,
             "parent_process_name": self.parent_process_name,
             "parent_process_hash": self.parent_process_hash,
-            "process_arguments": self.process_arguments
+            "process_commandline": self.process_commandline,
+            "process_name": self.process_name,
+            "process_hash": self.process_hash,
+            "hostname": self.hostname
         }
+
+    def get_kql_repr():
+        return (
+            "ProcessEvents",
+            {
+                "creation_time":"string",
+                "parent_process_name":"string",
+                "parent_process_hash":"string",
+                "process_commandline":"string",
+                "process_name":"string",
+                "process_hash":"string",
+                "hostname":"string"
+            }
+        )
---
+++ app/server/modules/helpers/constants.py
@@ -0,0 +1,39 @@
+#This file contains constants used throughout the game 
+
+LEGIT_FILES={
+	"cmd.exe":"d7ab69fad18d4a643d84a271dfc0dbdf",
+	"powershell.exe":"cda48fc75952ad12d99e526d0b6bf70a"
+	"svchost.exe":"9520a99e77d6196d0d09833146424113"
+	"rundll32.exe":"737978cd2171b0ea1de6691e24b7727f"
+	"wscript.exe":"ff00e0480075b095948000bdc66e81f0"
+	"msiexec.exe":"e5da170027542e25ede42fc54c929077"
+	"mshta.exe":"abdfc692d9fe43e2ba8fe6cb5a8cb95a"
+	"explorer.exe":"bf28f45b6cc2b125a10c0f7cf4affdad"
+	"runtimebroker.exe":"b2050c10311377b83b278e9514d7fdfa"
+	"reg.exe":"ecb768001dc8424e9b1ff3ac1e89c937"
+	"psexec.exe":"c5d86249c8053397ab9b514d887ddbdc"
+	"certutil.exe":"de9c75f34f47b60a71bba03760f0579e"
+	"wmic.exe":"631d1967c3065c581a78cc873dda08f6"
+	"regsvr32.exe":"b0c2fa35d14a9fad919e99d9d75e1b9e"
+	"schtasks.exe":"a6a56567b9859a0d147c898cecb9aaae"
+	"chrome.exe":"d8a86cbbd73840c97f9569a686057944"
+	"bitsadmin.exe":"539c7cf53b0158e6dec1e6aef9fb6c14"
+	"whoami.exe":"0b87f0034c42f13a0c843f862d7a3875"
+	"tasklist.exe":"80036c62eaceefac7f7df133634321f5"
+	"teams.exe":"43a140f9016bb99abccbcb455e77c574"
+	"squirrel.exe":"6f4893f0ff0fb87d8a2fe0be84f13367"
+	"lsass.exe":"5373e4594a071fe6031ad481cd23e910"
+	"cscript.exe":"79e4fbfe24a81b3a2aeb3b3d3deb3d75"
+	"dllhost.exe":"6f3c9485f8f97ac04c8e43ef4463a68c"
+	"taskhostw.exe":"9b805d9045970bedcac0c621a59c9348"
+	"winword.exe":"58ac73925a043dd951dc39f7126b5435"
+	}
+
+MALICIOUS_FILES={
+	"emotetlure.docm":"41b77100f8a7a971bc1beba6aeb58e77"
+	"emotet.exe":"ac9ce2b9a0df000a1730539ec264f48b"
+	"cobaltstrike.exe":"d053d4cd461951966e47ea44d28b42f8"
+	"sharphound.exe":"7d9213f8f3cba4035542eff1c9dbb341"
+	"mimikatz.exe":"6c9ad4e67032301a61a9897377d9cff8"
+	"advancedipscanner.exe":"597de376b1f80c06d501415dd973dcec"
+}
